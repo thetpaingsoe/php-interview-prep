02080bafedb581ef18b92f134068dc9f1dcf4f1c
Thet Paing Soe <thetpaingsoe.ptn@gmail.com>
2025-06-27 19:03:36 +0700

    Added ReadMe

---
+++ ReadMe.md
@@ -0,0 +1,13 @@
+# PHP Interview Prep 
+
+## 🛠️ Installation
+- Clone the project
+- Download packages using ( composer install ) 
+
+## 🏁 Running Project
+- `php -S localhost:8000 -t public`
+
+## 🧪 Unit Test 
+- In-order to run the test you can run using `phpunit` to run all the test cases.
+- If you want to run individual test case, please add the file path behind the command `phpunit --filter {ClassName}`
+- All our test cases will be under `test/`
--- src/View/MainView.php
+++ src/View/MainView.php
@@ -6,9 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
   </head>
-  <body>
-    <div class="container mx-auto mt-14 text-sky-900">
-      <?php echo $result ?>
+  <body class='min-h-full'>
+    <div class="container mx-auto max-w-7xl text-sky-900 border-x px-4
+     border-gray-300 border-dashed min-h-screen">
+      <div class=" pt-14"><?php echo $result ?></div>
     </div>
   </body>
 </html>
